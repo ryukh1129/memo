45d04841010e1c5a57bf2550861c1cb07ea37ed7
ryukh1129 <ryukh1129@gmail.com>
2026-01-19 10:30:12 +0900

    refactor: Memo Update 수정 기능 JDBC로 연결 코드로 변경

--- src/main/java/com/meta/memo/controller/MemoController.java
+++ src/main/java/com/meta/memo/controller/MemoController.java
@@ -66,20 +66,22 @@ public class MemoController {
         });
         return memoResponseDtoList;
     }
-//
-//    @PutMapping("{id}")
-//    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto memoRequestDto) {
-//        // 해당 id의 메모가 데이터베이스에 존재하는지 확인
-//        if (memoList.containsKey(id)) {
-//            // true면, 해당 메모 가져오기
-//            Memo foundMemo = memoList.get(id);
-//            foundMemo.update(memoRequestDto);
-//
-//            return foundMemo.getId();
-//        } else {
-//            throw new IllegalArgumentException("선택한 id의 메모는 존재하지 않습니다.");
-//        }
-//    }
+
+    @PutMapping("{id}")
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto memoRequestDto) {
+        // 해당 id의 메모가 존재하는지 확인
+        Memo foundMemo = findById(id);
+
+        // 해당 id의 메모가 데이터베이스에 존재하는지 확인
+        if (foundMemo != null) {
+            String sql = "UPDATE memo SET username = ?, contents = ? WHERE id = ?";
+            jdbcTemplate.update(sql, memoRequestDto.getUsername(), memoRequestDto.getContents(), id);
+
+            return id;
+        } else {
+            throw new IllegalArgumentException("선택한 id의 메모는 존재하지 않습니다.");
+        }
+    }
 //
 //    @DeleteMapping("{id}")
 //    public Long deleteMemo(@PathVariable Long id) {
@@ -92,4 +94,21 @@ public class MemoController {
 //            throw new IllegalArgumentException("선택한 id의 메모는 존재하지 않습니다.");
 //        }
 //    }
+
+    // 특정 id의 존재 여부 확인 공용 메서드
+    private Memo findById(Long id) {
+        // DB 조회
+        String sql = "SELECT * FROM memo WHERE id = ?";
+
+        return jdbcTemplate.query(sql, resultSet -> {
+            if(resultSet.next()) {
+                Memo memo = new Memo();
+                memo.setUsername(resultSet.getString("username"));
+                memo.setContents(resultSet.getString("contents"));
+                return memo;
+            } else {
+                return null;
+            }
+        }, id);
+    }
 }
