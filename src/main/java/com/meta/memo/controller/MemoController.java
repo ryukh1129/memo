ee68d2f5b80f840d3442c154221f5ac9980df7b5
ryukh1129 <ryukh1129@gmail.com>
2026-01-19 10:37:04 +0900

    refactor: Memo Delete 삭제 기능 JDBC 연결 코드로 변경

--- src/main/java/com/meta/memo/controller/MemoController.java
+++ src/main/java/com/meta/memo/controller/MemoController.java
@@ -72,7 +72,7 @@ public class MemoController {
         // 해당 id의 메모가 존재하는지 확인
         Memo foundMemo = findById(id);
 
-        // 해당 id의 메모가 데이터베이스에 존재하는지 확인
+        // 메모 내용 수정
         if (foundMemo != null) {
             String sql = "UPDATE memo SET username = ?, contents = ? WHERE id = ?";
             jdbcTemplate.update(sql, memoRequestDto.getUsername(), memoRequestDto.getContents(), id);
@@ -82,18 +82,22 @@ public class MemoController {
             throw new IllegalArgumentException("선택한 id의 메모는 존재하지 않습니다.");
         }
     }
-//
-//    @DeleteMapping("{id}")
-//    public Long deleteMemo(@PathVariable Long id) {
-//        // 해당 id의 메모가 데이터베이스에 존재하는지 확인
-//        if (memoList.containsKey(id)) {
-//            // true면, 해당 메모 삭제
-//            memoList.remove(id);
-//            return id;
-//        } else {
-//            throw new IllegalArgumentException("선택한 id의 메모는 존재하지 않습니다.");
-//        }
-//    }
+
+    @DeleteMapping("{id}")
+    public Long deleteMemo(@PathVariable Long id) {
+        // 해당 id의 메모가 존재하는지 확인
+        Memo foundMemo = findById(id);
+
+        // 메모 내용 삭제
+        if (foundMemo != null) {
+            String sql = "DELETE FROM memo WHERE id = ?";
+            jdbcTemplate.update(sql, id);
+
+            return id;
+        } else {
+            throw new IllegalArgumentException("선택한 id의 메모는 존재하지 않습니다.");
+        }
+    }
 
     // 특정 id의 존재 여부 확인 공용 메서드
     private Memo findById(Long id) {
